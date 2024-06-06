3dfd7709f423c34f6b3797a177dfdb992b369ff1
aslyu <kopolartec.artem@gmail.com>
2024-06-06 17:00:22 +0300

    update

---
+++ client/apps/web/src/app/providers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Providers({children}: { children: React.ReactNode }) {
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}
+
+export default Providers;
