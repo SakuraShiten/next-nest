39902bbb26a9d8fbf931aa76d568d69d0ccb1866
aslyu <kopolartec.artem@gmail.com>
2024-06-11 12:47:13 +0300

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
+import fastifyCors from '@fastify/cors';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
@@ -18,16 +19,10 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('openapi', app, document)
 
-    app.enableCors({
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        allowedHeaders: '*',
-        exposedHeaders: '*',
-        credentials: true
-    })
-
+    await app.register(fastifyCors, {
+        origin: true,
+        methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    });
 
 
     await app.listen(5000)
