9c3614b8b91a6efe1de40a062193addfb63fe307
aslyu <kopolartec.artem@gmail.com>
2024-06-11 10:13:31 +0300

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -25,9 +25,9 @@ import {Headers} from "@/models/elements/models/headers.model";
         TypeOrmModule.forRoot({
             type: 'mysql',
             host: 'localhost',
-            port: 3306,
+            port: 5432,
             username: 'root',
-            password: '',
+            password: '86HE1CA2BWGYCPeiB37I0E5mtWxUPBzXJkELy02e6F6Zqqmw23z1ryTInEQweUjj',
             database: 'test',
             synchronize: true,
             entities: [
