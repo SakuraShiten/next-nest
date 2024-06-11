91af3d17a48e208851bc148a3db646c9301f2881
aslyu <kopolartec.artem@gmail.com>
2024-06-11 12:38:35 +0300

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,15 +2,17 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
+import fastifyCors from "@fastify/cors";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter()
     )
-    app.enableCors({
-        origin: ['*']
-    })
+    app.register(fastifyCors, {
+        origin: true, // Разрешить все источники
+        methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    });
     app.setGlobalPrefix('api')
 
     const config = new DocumentBuilder()
